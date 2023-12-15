5196ef0d3404a8019b4806fff1ce6ff6263228bd
lightspd <82216756+Nithin0104@users.noreply.github.com>
2023-12-16 00:47:50 +0530

    Update http-common.js

--- client/src/services/http-common.js
+++ client/src/services/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://odm-lwebsite.vercel.app/api/v1/website',
+  baseURL: 'http://odm-lwebsite.vercel.app/api/v1/website/',
   headers: {
     'Content-type': 'application/json'
   }
