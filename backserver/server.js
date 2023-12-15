8904a2fdcd36135f43024f2a107da6ce93200493
lightspd <82216756+Nithin0104@users.noreply.github.com>
2023-12-16 00:56:56 +0530

    Update server.js

--- backserver/server.js
+++ backserver/server.js
@@ -6,6 +6,8 @@ const app = express()
  
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use("/api/v1/website", website)
 
